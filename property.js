297d6a1128cd4c7e25760bcc7415e83d673397b2
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-02-01 22:51:44 +0600

    object properties

---
+++ property.js
@@ -0,0 +1,25 @@
+const person = {
+    name: 'sodor uddin',
+    age: 25,
+    profession: 'developer',
+    salary: 25000,
+    married: true,
+    'fav places': ['bandorban', 'saintmartine', 'kuakata']
+}
+
+// console.log(person)
+
+// dot notation
+// console.log(person.profession)
+const income = person.salary;
+// console.log(income)
+
+// bracket notation
+// third bracket diye access kora
+console.log(person['age'])
+const boyos = person['age']
+console.log(boyos)
+
+// error
+// console.log(person.'fav places')
+console.log(person['fav places'])
